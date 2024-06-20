89f2d12d890f543d68b2648d6e19c9c702c70324
Karlito14 <leiroz26@hotmail.com>
2024-06-20 15:17:01 +0200

    ajout du footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import style from './style.module.scss';
 
 export const Footer = () => {
-    return (
-        <footer className={style.footer}>Footer</footer>
-    )
-}
+  return (
+    <footer className={style.footer}>
+      <p>Copyright © 2024 CookChef, Inc.</p>
+    </footer>
+  );
+};
