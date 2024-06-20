e8d1f69c0013bd7af8a11cd7e7611e0501b0f1e0
Karlito14 <leiroz26@hotmail.com>
2024-06-20 15:10:07 +0200

    réalisation du visuel du header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
 import style from './style.module.scss';
+import { FaBars, FaBasketShopping } from 'react-icons/fa6';
+import logo from '../../assets/images/cookchef.png';
 
 export const Header = () => {
-    return (
-        <header className={style.header}>Header</header>
-    )
-}
+  return (
+    <header className={style.header}>
+      <FaBars className={style.header__icon} />
+      <div className={style.header__containerImg}>
+        <img src={logo} alt="cookchef" className={style.header__containerImg__img} />
+      </div>
+      <ul className={style.header__list}>
+        <li className={style.header__list__item}>
+            <button className='btn btn-reverse-primary'>{<FaBasketShopping />}Panier</button>
+        </li>
+        <li className={style.header__list__item}>
+            <button className='btn btn-primary'>Connexion</button>
+        </li>
+      </ul>
+    </header>
+  );
+};
