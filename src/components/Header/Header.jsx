d74cd48308ab8a24f414612176d83e2334df8d38
Karlito14 <leiroz26@hotmail.com>
2024-06-21 16:46:13 +0200

    gestion menu hamburger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,53 @@
 import style from './style.module.scss';
-import { FaBars, FaBasketShopping } from 'react-icons/fa6';
+import { FaBars } from 'react-icons/fa6';
 import logo from '../../assets/images/cookchef.png';
+import { FaHeart } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
+  const [click, setClick] = useState(false);
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <header className={style.header}>
-      <div className={style.header__containerImg}>
-        <img
-          src={logo}
-          alt="cookchef"
-          className={style.header__containerImg__img}
+    <>
+      <header className={style.header}>
+        <div className={style.header__containerImg}>
+          <img
+            src={logo}
+            alt="cookchef"
+            className={style.header__containerImg__img}
+          />
+        </div>
+        <ul className={`${style.header__list} ${click && windowWidth < 640 ? style.active : ''}`}>
+          <li className={style.header__list__item}>
+            <button className="btn btn-reverse-primary">
+              {<FaHeart />}WishList
+            </button>
+          </li>
+          <li className={style.header__list__item}>
+            <button className="btn btn-primary">Connexion</button>
+          </li>
+        </ul>
+        <FaBars
+          className={style.header__icon}
+          onClick={() => setClick(!click)}
         />
-      </div>
-      <FaBars className={style.header__icon} />
-      <ul className={style.header__list}>
-        <li className={style.header__list__item}>
-          <button className="btn btn-reverse-primary">
-            {<FaBasketShopping />}WishList
-          </button>
-        </li>
-        <li className={style.header__list__item}>
-          <button className="btn btn-primary">Connexion</button>
-        </li>
-      </ul>
-    </header>
+      </header>
+      {click && windowWidth < 640 ? (
+        <div onClick={() => setClick(false)} className={style.calc}></div>
+      ) : null}
+    </>
   );
 };
