28f9c1f0ef9717f695e04531f45f68ba196b692e
Karlito14 <leiroz26@hotmail.com>
2024-06-21 17:14:08 +0200

    ajout fonction pour filter les recettes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,40 @@
 import { Recipe } from '../Recipe/Recipe';
 import style from './style.module.scss';
 import { data } from '../../data/data';
+import { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 export const Main = () => {
+  const [recipes, setRecipes] = useState(data);
+
+  const handleInput = (event) => {
+    const value = event.target.value.trim().toLowerCase();
+    const recipesUpdated = data.filter((recipe) =>
+      recipe.title.toLowerCase().startsWith(value)
+    );
+    setRecipes(recipesUpdated);
+  };
+
   return (
     <div className={style.container}>
       <h1 className={style.container__title}>Découvrez nos recettes</h1>
+      <div className={style.container__divInput}>
+        <FaSearch className={style.container__divInput__icon} />
+        <input
+          type="text"
+          placeholder="Rechercher"
+          className={style.container__divInput__input}
+          onInput={handleInput}
+        />
+      </div>
       <main className={style.container__main}>
         <ul className={style.container__main__list}>
-          {data.map((recipe) => (
-            <Recipe key={recipe._id} title={recipe.title} image={recipe.image} />
+          {recipes.map((recipe) => (
+            <Recipe
+              key={recipe._id}
+              title={recipe.title}
+              image={recipe.image}
+            />
           ))}
         </ul>
       </main>
