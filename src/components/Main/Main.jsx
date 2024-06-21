5e2b001a7eed25ac57c811c778ab7bacac0b8a6c
Karlito14 <leiroz26@hotmail.com>
2024-06-21 10:54:14 +0200

    ajout icone coeur et possibilité de liker une recette

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,12 +8,8 @@ export const Main = () => {
       <h1 className={style.container__title}>Découvrez nos recettes</h1>
       <main className={style.container__main}>
         <ul className={style.container__main__list}>
-          {data.map((recipe) => (
-            <Recipe
-              key={recipe.title}
-              title={recipe.title}
-              image={recipe.image}
-            />
+          {data.map((recipe, index) => (
+            <Recipe key={index} title={recipe.title} image={recipe.image} />
           ))}
         </ul>
       </main>
--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import style from './style.module.scss';
+import { FaHeart } from 'react-icons/fa';
 
 export const Recipe = ({ title, image }) => {
+  const [like, setLike] = useState(false);
+
+  const handleClickHeart = (e) => {
+    e.stopPropagation();
+    setLike(!like);
+  };
+
   return (
-    <li>
+    <li onClick={handleClickHeart}>
       <figure className={style.recipe}>
         <img className={style.recipe__img} src={image} alt="recette" />
         <figcaption className={style.recipe__title}>
           <h3>{title}</h3>
+          <FaHeart
+            className={style.recipe__title__icon}
+            style={{ color: like && '#ff6348' }}
+          />
         </figcaption>
       </figure>
     </li>
