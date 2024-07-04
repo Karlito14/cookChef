ed8a952e0cafd05d2bcf8eff67292fda2722ef7c
Karlito14 <leiroz26@hotmail.com>
2024-07-04 16:46:57 +0200

    ajout variable env

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb+srv://leiroz26:hustler14@cluster0.ivycwjk.mongodb.net/cookchef?retryWrites=true&w=majority&appName=Cluster0')
+  .connect(`mongodb+srv://leiroz26:${process.env.PASSWORD_DATABASE}@cluster0.ivycwjk.mongodb.net/cookchef?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => {
     console.log('connexion DB OK');
   })
